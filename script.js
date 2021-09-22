a58b4b50ffcc73cc8abbd54615c1c47492d452a1
Maomurillo4 <maomurillo453@gmail.com>
2021-09-21 19:24:56 -0500

    Primer Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const huTao1 = () =>{
+    let nombre = "Hu Tao HD";
+    alert(nombre);
+}
+
+const huTao2 = () =>{
+    let nombre = "Hu Tao Chikita";
+    alert(nombre);
+}
+
+const huTao3 = () =>{
+    let nombre = "Hu Tao . _.";
+    alert(nombre);
+}
+
+const ganyu1 = () =>{
+    let nombre = "Ganyu HD";
+    alert(nombre);
+}
+
+const ganyu2 = () =>{
+    let nombre = "Ganyu Kiut";
+    alert(nombre);
+}
+
+const ganyu3 = () =>{
+    let nombre = "Ganyu con cara de >:o";
+    alert(nombre);
+}
